4b22b5040a2d5bcaaef78073085880c1188abf4e
bellenger-l <bellenger_lea_31@hotmail.fr>
2018-10-31 13:50:30 +0100

    Lint Rscript with lintr

--- R/normalization/normalization_singleCells.R
+++ R/normalization/normalization_singleCells.R
@@ -3,27 +3,48 @@
 ## Setup R error handling to go to stderr
 options(
   show.error.messages = F,
-  error = function () {
+  error = function (){
     cat(geterrmessage(), file = stderr())
     q("no", 1, F)
   }
 )
 
 option_list <- list(
-  optparse::make_option(c("-f", "--file"), type="character", help="Path to the input file"),
-  optparse::make_option(c("-c", "--colnames"), type="logical", help="Consider first line as header"),
-  optparse::make_option(c("-s", "--sep"), type="character", help="Input file separator"),
-  optparse::make_option(c("-m", "--method"), type="character", help="Name of the method/package used for the normalization : scater or Seurat"),
-  optparse::make_option("--min_mean", type="character", help="Minimum average count of genes to be used for normalization with scater"),
-  optparse::make_option("--size_factor", type="logical", help="Using size factors based on deconvolution from cell pools (TRUE) or considering library sizes as size Factors (FALSE) ? NB: Mandatory if there is less than 100 cells in dataset, you must choose library sizes"),
-  optparse::make_option(c("-o", "--output_matrix"), type = "character", help="Path to the filtered matrix")
+  optparse::make_option(c("-f", "--file"),
+                        type = "character",
+                        help = "Path to the input file"),
+  optparse::make_option(c("-c", "--colnames"),
+                        type = "logical", help =
+                          "Consider first line as header"),
+  optparse::make_option(c("-s", "--sep"),
+                        type = "character",
+                        help = "Input file separator"),
+  optparse::make_option(c("-m", "--method"),
+                        type = "character",
+                        help = "Name of the method/package used for the
+                        normalization : scater or Seurat"),
+  optparse::make_option("--min_mean",
+                        type = "character",
+                        help = "Minimum average count of genes to be used
+                        for normalization with scater"),
+  optparse::make_option("--size_factor",
+                        type = "logical",
+                        help = "Using size factors based on deconvolution
+                        from cell pools (TRUE) or considering library sizes
+                        as size Factors (FALSE) ? NB: Mandatory if there is
+                        less than 100 cells in dataset, you must choose library
+                        sizes"),
+  optparse::make_option(c("-o", "--output_matrix"),
+                        type = "character",
+                        help = "Path to the filtered matrix")
 )
 
-parser <- optparse::OptionParser(usage = "%prog [options] file", option_list = option_list)
-args = optparse::parse_args(parser)
+parser <- optparse::OptionParser(usage = "%prog [options] file",
+                                 option_list = option_list)
+args <- optparse::parse_args(parser)
 
 ## Import dataset
-data.counts = read.table(
+data.counts <- read.table(
   args$file,
   header = args$colnames,
   stringsAsFactors = F,
@@ -33,29 +54,32 @@ data.counts = read.table(
 )
 
 ## Checking parameters
-if(ncol(data.counts) < 100 & args$size_factor == TRUE){
-  stop("You need at least 100 cells in your dataset to use the deconvolution method.")
+if (ncol(data.counts) < 100 & args$size_factor == TRUE){
+  stop("You need at least 100 cells in your dataset
+       to use the deconvolution method.")
 }
 
-if (args$method == "scater") {
-  sce = SingleCellExperiment::SingleCellExperiment(assays = list(counts = as.matrix(data.counts)))
-  if (args$size_factor == TRUE) {
-    sce = scran::computeSumFactors(sce,
-                                   min.mean = args$min_mean,
-                                   sf.out = FALSE,
-                                   get.spikes = FALSE,
-                                   assay.type = "counts")
+if (args$method == "scater"){
+  sce <- SingleCellExperiment::SingleCellExperiment(
+    assays = list(counts = as.matrix(data.counts)))
+  if (args$size_factor == TRUE){
+    sce <- scran::computeSumFactors(
+      sce,
+      min.mean = args$min_mean,
+      sf.out = FALSE,
+      get.spikes = FALSE,
+      assay.type = "counts"
+    )
   }
-  sce = scater::normalize(sce)
-  
-  normalized_data_counts = SingleCellExperiment::logcounts(sce)
+  sce <- scater::normalize(sce)
+  normalized_data_counts <- SingleCellExperiment::logcounts(sce)
 }
 
-if (args$method == "Seurat") {
+if (args$method == "Seurat"){
   sce <- Seurat::CreateSeuratObject(raw.data = data.counts)
-  sce <- Seurat::NormalizeData(sce, scale.factor = median(sce@meta.data$nUMI))
-  
-  normalized_data_counts = as.matrix(sce@data)
+  sce <- Seurat::NormalizeData(sce,
+                               scale.factor = median(sce@meta.data[, "nUMI"]))
+  normalized_data_counts <- as.matrix(sce@data)
 }
 
 write.table(
@@ -65,4 +89,4 @@ write.table(
   quote = F,
   col.names = T,
   row.names = T
-)  
+)
